bebcf6389ed381bbca3a75512590cfc18dc6e16c
Grzegorz <grzegorz.popiel@yahoo.com>
2020-06-21 12:12:45 +0200

    Code refractoring.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import './App.css';
-import CurrencyContainer from './app/components/CurrencyContainer'
-import TransactionContainer from "./app/components/TransactionContainer";
-import InputContainer from "./app/components/InputContainer";
+import {default as CurrencyContainer} from './app/components/CurrencyContainer'
+import {default as TransactionContainer} from "./app/components/TransactionContainer";
+import {default as InputContainer} from "./app/components/InputContainer";
 
 class App extends Component {
   render() {
